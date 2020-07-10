0468ab229e6bce021671934c4b0ccaf541bf2650
shun <1039849632@qq.com>
2020-07-09 23:23:32 -0700

    Initial Commit

---
+++ baisijie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  baisijie
+//
+//  Created by 小仙女 guluoyan.cn on 2020/7/9.
+//  Copyright © 2020 小仙女 guluoyan.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ baisijieTests/baisijieTests.m
@@ -0,0 +1,37 @@
+//
+//  baisijieTests.m
+//  baisijieTests
+//
+//  Created by 小仙女 guluoyan.cn on 2020/7/9.
+//  Copyright © 2020 小仙女 guluoyan.cn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface baisijieTests : XCTestCase
+
+@end
+
+@implementation baisijieTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
